0a537038de4970d5e6b2b827f046f36ecf8ccdf1
Josephjbliss <josephjbliss@gmail.com>
2016-01-21 14:22:48 -0500

    Updated archive

--- explorer/js/archive.js
+++ explorer/js/archive.js
@@ -4,41 +4,30 @@
   $('#load-more').on('click', function(evt) {
 
     evt.preventDefault();
-
-    // if($('#article-list article').length < 12) {
-      
-      $(this).prop('disabled', true).html('exploring the archive').append($('<i/>').addClass('fa fa-circle-o-notch fa-spin'));
-
-      $.getJSON('http://ga-challenge-api-production.herokuapp.com/explorer/posts', function(data) {
-
-        //timeout function to 'fake' server call
-        // setTimeout(function(){  
-          $.each(data.posts, function(key, value){
-            var newArticle = $('<article/>');
-            newArticle.append($('<i/>').addClass(value.category+' fa'));
-            newArticle.append($('<h2/>').html('From the Archive'));
-            newArticle.append($('<h1/>').html(value.title));
-            newArticle.append($('<h3/>').html(value.date));
-            newArticle.append($('<p/>').html(value.blurb));
-            $('#article-list footer').before(newArticle);
-            $('#load-more').prop('disabled', false).html('load more');
-          });
-
-        // }, 2000);
-
-      }).error(function() {
-
-          $('#load-more').prop('disabled', true).html('something went wrong').append($('<i/>').addClass('fa fa-exclamation-triangle'));
-      
-      });
-    // }
     
-    // else {
+    $(this).prop('disabled', true).html('exploring the archive').append($('<i/>').addClass('fa fa-circle-o-notch fa-spin'));
+
+    $.getJSON('http://api.credentials.generalassemb.ly/explorer/posts', function(data) {
+
+        $.each(data.posts, function(key, value){
+          var newArticle = $('<article/>');
+          newArticle.append($('<i/>').addClass(value.category+' fa'));
+          newArticle.append($('<h2/>').html('From the Archive'));
+          newArticle.append($('<h1/>').html(value.title));
+          newArticle.append($('<h3/>').html(value.date));
+          newArticle.append($('<p/>').html(value.blurb));
+          $('#article-list footer').before(newArticle);
+          $('#load-more').prop('disabled', false).html('load more');
+        });
+
+    }).error(function() {
+
+        $('#load-more').prop('disabled', true).html('something went wrong').append($('<i/>').addClass('fa fa-exclamation-triangle'));
+    
+    });
 
       // $(this).prop('disabled', true).html('end of archive');
     
-    // }
-
   });
 
 })();
