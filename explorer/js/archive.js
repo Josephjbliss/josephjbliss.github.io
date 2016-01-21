362bb1e112e2626c986b6257f8a43d64e7bed961
Josephjbliss <josephjbliss@gmail.com>
2016-01-21 14:08:48 -0500

    updated explorer

--- explorer/js/archive.js
+++ explorer/js/archive.js
@@ -5,14 +5,14 @@
 
     evt.preventDefault();
 
-    if($('#article-list article').length < 12) {
+    // if($('#article-list article').length < 12) {
       
       $(this).prop('disabled', true).html('exploring the archive').append($('<i/>').addClass('fa fa-circle-o-notch fa-spin'));
 
       $.getJSON('http://ga-challenge-api-production.herokuapp.com/explorer/posts', function(data) {
 
         //timeout function to 'fake' server call
-        setTimeout(function(){  
+        // setTimeout(function(){  
           $.each(data.posts, function(key, value){
             var newArticle = $('<article/>');
             newArticle.append($('<i/>').addClass(value.category+' fa'));
@@ -24,20 +24,20 @@
             $('#load-more').prop('disabled', false).html('load more');
           });
 
-        }, 2000);
+        // }, 2000);
 
       }).error(function() {
 
           $('#load-more').prop('disabled', true).html('something went wrong').append($('<i/>').addClass('fa fa-exclamation-triangle'));
       
       });
-    }
+    // }
     
-    else {
+    // else {
 
-      $(this).prop('disabled', true).html('end of archive');
+      // $(this).prop('disabled', true).html('end of archive');
     
-    }
+    // }
 
   });
 
