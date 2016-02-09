cdb442213ed1a106e3939260144edb5c1b54effc
Josephjbliss <josephjbliss@gmail.com>
2016-02-09 15:20:05 -0500

    Updated Photo Portfolio

--- photo-portfolio/js/project.js
+++ photo-portfolio/js/project.js
@@ -1,22 +1,6 @@
-$(document).ready(function(){
-  // var navFadeOut = function(){ 
-  //   $("header nav, header h1").stop().animate({opacity: 0}, 400);
-  // };
-
-  // var navFadeIn = function() {
-  //   $("header nav, header h1").stop().animate({opacity: 1}, 400); 
-  //   setTimeout(navFadeOut, 5000);
-  // }
-
-  // setTimeout(navFadeOut, 5000);
-
-  // $(window).on("scroll", navFadeIn);
-  // $(document).on("mousemove", navFadeIn);
-
-  $("#mobile").click(function(){
+$(document).ready(function() {
+  $("#mobile").click(function() {
     $("nav").toggleClass("open");
     $(this).toggleClass("fa-bars fa-close");
   });
 });
-
-
