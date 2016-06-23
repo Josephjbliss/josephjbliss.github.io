a385a6379eac55cf24f31b01590ce5a0003b059e
Josephjbliss <josephjbliss@gmail.com>
2016-06-23 00:56:37 -0400

    Updates

--- fewdashboard/js/project.js
+++ fewdashboard/js/project.js
@@ -1,5 +1,9 @@
 $(function() {
   'use strict';
 
+  $('.accordion-heading').on('click', function(){
+    $(this).next().slideToggle(500);
+    $(this).find('.fa').toggleClass('fa-plus').toggleClass('fa-minus');
+  });
 
 });
